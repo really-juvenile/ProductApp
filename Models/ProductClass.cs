9f7b71ad17fdd95a42ea198a3c3a8124a3498ebf
Arjun.Pawar <Arjun.Pawar@DELL1534>
2024-07-12 11:22:13 +0530

    Add project files.

---
+++ Models/ProductClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Models
+{
+    internal class ProductClass
+    {
+        private int _id;
+        private string _name;
+        private float _price;
+        private double _discountPercentage;
+
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public float price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public double discountPercentage
+        {
+            get { return _discountPercentage; }
+            set { _discountPercentage = value; }
+        }
+        public double GetDiscountedCost()
+        {
+             return _price - (_price * (_discountPercentage/100));
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductApp.Models;
+
+namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductClass product1 = new ProductClass();
+            product1.id= 1;
+            product1.name = "bag";
+            product1.price = 100;
+            product1.discountPercentage = 50;
+
+            ProductClass product2 = new ProductClass();
+            product2.id = 2;
+            product2.name = "lamp";
+            product2.price = 200;
+            product2.discountPercentage = 50;
+            DisplayProduct(product2);
+            DisplayProduct(product1);
+
+
+
+        }
+        static void DisplayProduct(ProductClass product)
+        {
+            Console.WriteLine("Product_id: "+ product.id);
+            Console.WriteLine("product_name: "+product.name);
+            Console.WriteLine("price: "+product.price);
+            Console.WriteLine("discount percentage: "+product.discountPercentage);
+            Console.WriteLine("price after discount" + product.GetDiscountedCost());
+
+        }
+    }
+}
